a2cc0042043abe6b0ac2591c39a80c923e81d767
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2024-03-05 12:20:41 -0600

    why does the version number not change

--- engine/src/uci.h
+++ engine/src/uci.h
@@ -49,7 +49,7 @@ void uci(ThreadInfo &thread_info, Position &position) {
     }
 
     else if (command == "uci") {
-      printf("id name Patricia 0.1\nid author Adam Kulju\noption name Hash "
+      printf("id name Patricia 1.0\nid author Adam Kulju\noption name Hash "
              "type spin default 32 min 1 max 131072\noption name Threads type "
              "spin default 1 min 1 max 1\nuciok\n");
     }
