8eeb0bd884befc771096f422e01f72861118bbe0
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2024-07-13 23:43:57 -0500

    update uci_elo max params

--- engine/src/uci.h
+++ engine/src/uci.h
@@ -61,8 +61,8 @@ void uci(ThreadInfo &thread_info, Position &position) {
           "id name Patricia 3\nid author Adam Kulju\n"
           "option name Hash type spin default 32 min 1 max 131072\n"
           "option name Threads type spin default 1 min 1 max 1024\n"
-          "option name UCI_Elo type spin default 3300 min 1100 max 3200\n"
-          "option name UCI_Limit type spin default 3300 min 1100 max 3200\n");
+          "option name UCI_Elo type spin default 3300 min 1100 max 3300\n"
+          "option name UCI_Limit type spin default 3300 min 1100 max 3300\n");
 
       for (auto &param : params) {
         std::cout << "option name " << param.name << " type spin default "
