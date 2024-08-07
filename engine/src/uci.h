4326efb2707fe76b54325033cda2f10a7c07f8bf
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2024-08-06 21:38:55 -0500

    fix uci version (i did it again)

--- engine/src/uci.h
+++ engine/src/uci.h
@@ -59,7 +59,7 @@ void uci(ThreadInfo &thread_info, Position &position) {
 
     else if (command == "uci") {
       printf(
-          "id name Patricia 3\nid author Adam Kulju\n"
+          "id name Patricia 3.1\nid author Adam Kulju\n"
           "option name Hash type spin default 32 min 1 max 131072\n"
           "option name Threads type spin default 1 min 1 max 1024\n"
           "option name Skill_Level type spin default 3000 min 500 max 3000\n"
