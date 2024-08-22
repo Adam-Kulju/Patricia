017dd9916e4d67d4f7b7479adb7a8d6ddde5fb16
Adhoniran Gomes <adhoniran@users.noreply.github.com>
2024-08-22 17:06:41 -0300

    Make functions and variables inline
    Changed multiple function and variable declarations to 'inline' to enable internal linkage and improve performance. This helps reduce duplicate symbol errors during linking and potentially increases compilation efficiency.

--- engine/src/params.h
+++ engine/src/params.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 
-MultiArray<int, MaxSearchDepth + 1, ListSize> LMRTable;
+inline MultiArray<int, MaxSearchDepth + 1, ListSize> LMRTable;
 
 
 struct Parameter {
@@ -13,7 +13,7 @@ struct Parameter {
   int min, max;
 };
 
-std::vector<Parameter> params;
+inline std::vector<Parameter> params;
 
 // SPSA parameter code is based off Clover
 // (https://github.com/lucametehau/CloverEngine)
@@ -32,33 +32,33 @@ struct CreateParam {
   CreateParam name(#name, value, min, max);
 
 
-TUNE_PARAM(NMPMinDepth, 3, 1, 5);
-TUNE_PARAM(NMPBase, 4, 1, 5);
-TUNE_PARAM(NMPDepthDiv, 6, 3, 9);
-TUNE_PARAM(NMPEvalDiv, 183, 100, 300);
-TUNE_PARAM(RFPMargin, 85, 50, 110);
-TUNE_PARAM(RFPMaxDepth, 10, 6, 12);
-TUNE_PARAM(LMRBase, 4, 2, 8);
-TUNE_PARAM(LMRRatio, 22, 15, 30);
-TUNE_PARAM(LMPBase, 2, 1, 5);
-TUNE_PARAM(LMPDepth, 6, 3, 7);
-TUNE_PARAM(SEDepth, 6, 4, 10);
-TUNE_PARAM(SEDoubleExtMargin, 19, 10, 30);
-TUNE_PARAM(FPDepth, 8, 5, 11);
-TUNE_PARAM(FPMargin1, 105, 50, 150);
-TUNE_PARAM(FPMargin2, 123, 75, 175);
-TUNE_PARAM(IIRMinDepth, 2, 1, 5);
-TUNE_PARAM(SeePruningDepth, 8, 5, 11);
-TUNE_PARAM(SeePruningQuietMargin, -83, -110, -50);
-TUNE_PARAM(SeePruningNoisyMargin, -33, -50, -10);
-TUNE_PARAM(HistBonus, 274, 200, 400);
-TUNE_PARAM(HistMax, 2537, 1500, 3500);
-TUNE_PARAM(AgeDiffDiv, 5, 2, 6);
-TUNE_PARAM(OldBonusMult, 22, 10, 30);
-TUNE_PARAM(NewBonusMult, 33, 20, 40);
+inline TUNE_PARAM(NMPMinDepth, 3, 1, 5);
+inline TUNE_PARAM(NMPBase, 4, 1, 5);
+inline TUNE_PARAM(NMPDepthDiv, 6, 3, 9);
+inline TUNE_PARAM(NMPEvalDiv, 183, 100, 300);
+inline TUNE_PARAM(RFPMargin, 85, 50, 110);
+inline TUNE_PARAM(RFPMaxDepth, 10, 6, 12);
+inline TUNE_PARAM(LMRBase, 4, 2, 8);
+inline TUNE_PARAM(LMRRatio, 22, 15, 30);
+inline TUNE_PARAM(LMPBase, 2, 1, 5);
+inline TUNE_PARAM(LMPDepth, 6, 3, 7);
+inline TUNE_PARAM(SEDepth, 6, 4, 10);
+inline TUNE_PARAM(SEDoubleExtMargin, 19, 10, 30);
+inline TUNE_PARAM(FPDepth, 8, 5, 11);
+inline TUNE_PARAM(FPMargin1, 105, 50, 150);
+inline TUNE_PARAM(FPMargin2, 123, 75, 175);
+inline TUNE_PARAM(IIRMinDepth, 2, 1, 5);
+inline TUNE_PARAM(SeePruningDepth, 8, 5, 11);
+inline TUNE_PARAM(SeePruningQuietMargin, -83, -110, -50);
+inline TUNE_PARAM(SeePruningNoisyMargin, -33, -50, -10);
+inline TUNE_PARAM(HistBonus, 274, 200, 400);
+inline TUNE_PARAM(HistMax, 2537, 1500, 3500);
+inline TUNE_PARAM(AgeDiffDiv, 5, 2, 6);
+inline TUNE_PARAM(OldBonusMult, 22, 10, 30);
+inline TUNE_PARAM(NewBonusMult, 33, 20, 40);
 
 
-void print_params_for_ob() {
+inline void print_params_for_ob() {
   for (auto &param : params) {
     std::cout << param.name << ", int, " << param.value << ", " << param.min
               << ", " << param.max << ", "
@@ -66,7 +66,7 @@ void print_params_for_ob() {
   }
 }
 
-void init_LMR() {
+inline void init_LMR() {
   for (int i = 0; i < MaxSearchDepth; i++) {
     for (int n = 0; n < ListSize; n++) {
       LMRTable[i][n] = LMRBase / 10.0 + log(i) * log(n) / (LMRRatio / 10.0);
