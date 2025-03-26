a42d483ee1d2a9377587c37d603a0e450e9bccfd
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2025-03-25 21:41:37 -0500

    make lmr less sketchy
    Bench: 4177126

--- engine/src/search.h
+++ engine/src/search.h
@@ -776,7 +776,7 @@ int search(int alpha, int beta, int depth, bool cutnode, Position &position,
 
 
       // Clamp reduction so we don't immediately go into qsearch
-      R = std::clamp(R, 1, newdepth - 1);
+      R = std::clamp(R, 1, newdepth);
 
       // Reduced search, reduced window
       score = -search<false>(-alpha - 1, -alpha, newdepth - R + 1, true,
