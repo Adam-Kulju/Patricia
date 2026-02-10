000c3d893e982674dd416d2f33dd00f622d94a1f
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2026-02-09 20:28:44 -0600

    New filter script

---
+++ utils/position_filter_10.cpp
@@ -0,0 +1,68 @@
+//Development
+//Pieces on the first two ranks count as poorly developed (be7 can be strong later but isn't really now)
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include "base.h"
+
+int mat(board_info *board){
+  int mat = 0, total_mat = 0;
+  int piece_values[5] = {100, 300, 300, 500, 900};
+  for (int i = 0; i < 5; i++){
+    mat += (board->material_count[Colors::White][i] - board->material_count[Colors::Black][i]) * piece_values[i];
+    total_mat += (board->material_count[Colors::White][i] + board->material_count[Colors::Black][i]) * piece_values[i];
+  }
+  return total_mat < 3000 ? 10000 : mat;
+}
+
+
+
+int filter(const std::string input, const std::string &output){
+  char buffer[32768];
+  int buffer_key = 0;
+  std::ofstream fout(output);
+  std::ifstream fin(input);
+  std::string line;
+  int total_lines = 0, filtered_lines = 0;
+  while (std::getline(fin, line)){
+    total_lines++;
+    board_info board;
+    bool color = setfromfen(&board, line.c_str());
+
+    //After setting up the board, our next task is to extract the evaluation and game result of the position.
+    int eval_delimiter = line.find(" | ") + 3;
+
+    int eval = atoi(line.substr(eval_delimiter, line.find(" | ", eval_delimiter) - eval_delimiter).c_str());
+    float result = atof(line.substr(line.rfind(" | ") + 2).c_str());
+
+    eval *= color == Colors::White ? 1 : -1;
+
+    int material = mat(&board) * (color == Colors::White ? 1 : -1);
+
+    if (abs(material) != 10000 && 
+    ((eval > material + 400 && material < 100 && result == 1) || (eval < material - 400 && material > -100 && result == 0)))
+    {
+      filtered_lines++;
+      buffer_key += sprintf(buffer + buffer_key, "%s\n", line.c_str());
+      if (buffer_key > 32668){
+        fout << buffer;
+        buffer_key = 0;
+      }
+    }
+    
+  }
+
+  fout.close();
+  fin.close();
+  printf("%i positions read in, %i filtered\n", total_lines, filtered_lines);
+  return 0;
+}
+
+
+int main(int argc, char *argv[]){
+  clock_t a = clock();
+  filter(argv[1], argv[2]);
+  printf("%f seconds\n", (float)(clock() - a) / CLOCKS_PER_SEC);
+  return 0;
+}
