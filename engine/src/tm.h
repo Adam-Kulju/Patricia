8b5fd5973c78b307ea9e847c9a050776f4f31bac
Adam Kulju <85965318+Adam-Kulju@users.noreply.github.com>
2026-06-21 16:31:46 -0500

    dont reduce ratio
    Bench: 3015693

--- engine/src/tm.h
+++ engine/src/tm.h
@@ -6,8 +6,8 @@ void adjust_soft_limit(ThreadInfo &thread_info, uint64_t best_move_nodes, int bm
   double fract = (double)best_move_nodes / thread_info.nodes;
   double factor = (NodeTmFactor1 / 100.0f - fract) * NodeTmFactor2 / 100.0f;
   double bm_factor = BmFactor1 / 100.0f - (bm_stability * 0.06);
-  double score_factor = std::clamp(1.0 - (double)score_diff * ScoreTmScale / 10000.0,
-                                   ScoreTmMin / 100.0, ScoreTmMax / 100.0);
+  double score_factor = std::clamp(1.0 - (double)std::min(score_diff, 0) * ScoreTmScale / 10000.0,
+                                   1.0, ScoreTmMax / 100.0);
 
   thread_info.opt_time = std::min<uint32_t>(thread_info.original_opt * factor * bm_factor * score_factor,
                                             thread_info.max_time);
